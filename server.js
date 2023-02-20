2e92b6055e7df0c2fb925227f64d242a28da0b77
Joaquin Akerman <joaquin_akerman@hotmail.com>
2023-02-20 16:16:33 -0300

    Update variables to English for better consistency

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
-
 const app = express();
 
-// Configuración del motor de plantillas
+//import all the functions from the controller
+const clubsController = require('./controllers/clubsController');
+
+let clubsDataBase = JSON.parse(fs.readFileSync('./clubs.json', 'utf-8'));
+function consoleLog(parameter) {
+  console.log(parameter + 'ejecuted ');
+}
+
 app.engine(
   'handlebars',
   handlebars({
@@ -18,44 +24,62 @@ app.engine(
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
-// Configuración del middleware para procesar los datos enviados desde el formulario
+// Setting up the middleware to process the data sent from the form
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Configuración de la ruta raíz para mostrar todos los clubes
+// Setting up the root route to display all clubs
 app.get('/', (req, res) => {
-  fs.readFile('clubes.json', (err, data) => {
-    if (err) throw err;
-    const clubes = JSON.parse(data);
-    res.render('clubes', { clubes });
-  });
+  consoleLog(' show all clubs'),
+    fs.readFile('clubs.json', (err, data) => {
+      if (err) throw err;
+      const clubs = JSON.parse(data);
+      res.render('clubs', { clubs });
+    });
 });
 
-// Configuración de la ruta para mostrar un club en particular
-app.get('/clubes/:id', (req, res) => {
+//Setting up the route to display the form for creating a new club
+app.get('/clubs/new', (req, res) => {
+  consoleLog('create a new club');
+  res.render('new');
+});
+// Setting up the route to display a particular club
+app.get('/clubs/:id', (req, res) => {
+  consoleLog(' show a particular club');
   const id = req.params.id;
-  fs.readFile('clubes.json', (err, data) => {
+  fs.readFile('clubs.json', (err, data) => {
     if (err) throw err;
-    const clubes = JSON.parse(data);
-    const club = clubes.find((c) => c.id == id);
+    const clubs = JSON.parse(data);
+    const club = clubs.find((c) => c.id == id);
     if (club) {
       res.render('club', { club });
     } else {
-      res.status(404).send('Club no encontrado');
+      res.status(404).send('Club not found');
     }
   });
 });
 
-// Configuración de la ruta para mostrar el formulario de creación de un nuevo club
-app.get('/crear', (req, res) => {
-  res.render('crear');
+//Setting up the route to display the form for editing an existing club
+app.get('/clubs/edit/:id', async (req, res) => {
+  console.log('show the form for editing an existing club');
+  try {
+    const id = parseInt(req.params.id); // convert string to number
+    const club = clubsDataBase.find((obj) => obj.id === id);
+    res.render('edit', { club });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('error to show the form for editing an existing club');
+  }
 });
 
-// Configuración de la ruta para procesar el envío del formulario de creación de un nuevo club
-app.post('/crear', (req, res) => {
-  fs.readFile('clubes.json', (err, data) => {
+//////////////////////POST////////////////////////
+
+// Config of the route to process the creation of a new club
+app.post('/clubs/new', (req, res) => {
+  consoleLog('process the creation of a new club');
+  fs.readFile('clubs.json', (err, data) => {
     if (err) throw err;
-    const clubes = JSON.parse(data);
-    const nuevoClub = {
+    const clubs = JSON.parse(data);
+    const club = {
       id: uuidv4(),
       name: req.body.name,
       shortName: req.body.shortName,
@@ -70,33 +94,64 @@ app.post('/crear', (req, res) => {
       venue: req.body.venue,
       lastUpdated: new Date().toISOString(),
     };
-    clubes.push(nuevoClub);
-    fs.writeFile('clubes.json', JSON.stringify(clubes), (err) => {
+    clubs.push(club);
+    fs.writeFile('clubs.json', JSON.stringify(clubs), (err) => {
       if (err) throw err;
       res.redirect('/');
     });
   });
 });
 
-// Configuración de la ruta para mostrar el formulario de edición de un club existente
-app.get('/editar/:id', (req, res) => {
+// setting up the route to process the editing of an existing club
+app.post('/clubs/edit/:id', (req, res) => {
   const id = req.params.id;
-  fs.readFile('clubes.json', (err, data) => {
+  consoleLog('Updating the club with id:', id);
+  fs.readFile('clubs.json', (err, data) => {
     if (err) throw err;
-    const clubes = JSON.parse(data);
-    const club = clubes.find((c) => c.id == id);
+    const clubs = JSON.parse(data);
+    const club = clubs.find((c) => c.id == id);
     if (club) {
-      res.render('editar', { club });
+      club.id = req.body.id;
+      club.name = req.body.name;
+      club.shortName = req.body.shortName;
+      club.tla = req.body.tla;
+      club.crestUrl = req.body.crestUrl;
+      club.address = req.body.address;
+      club.phone = req.body.phone;
+      club.website = req.body.website;
+      club.email = req.body.email;
+      club.founded = parseInt(req.body.founded);
+      club.clubColors = req.body.clubColors;
+      club.venue = req.body.venue;
+      club.lastUpdated = new Date().toISOString();
+      fs.writeFile('clubs.json', JSON.stringify(clubs), (err) => {
+        if (err) throw err;
+        res.redirect('/');
+      });
     } else {
-      res.status(404).send('Club no encontrado');
+      res.status(404).send('Editing Club not found');
     }
   });
 });
 
-// Configuración de la ruta
+// Setting up the route to process the deletion of an existing club
+app.post('/eliminar/:id', (req, res) => {
+  consoleLog(' process the deletion of an existing club');
+
+  const id = req.params.id;
+  fs.readFile('clubs.json', (err, data) => {
+    if (err) throw err;
+    let clubs = JSON.parse(data);
+    clubs = clubs.filter((c) => c.id != id);
+    fs.writeFile('clubs.json', JSON.stringify(clubs), (err) => {
+      if (err) throw err;
+      res.redirect('/');
+    });
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor iniciado en el puerto ${PORT}`);
+  console.log(`Server started at port ${PORT}`);
 });
